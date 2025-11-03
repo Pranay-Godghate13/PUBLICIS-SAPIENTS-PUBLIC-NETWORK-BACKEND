0e2f9c2184fbe759837a7e7bfd1645accd9538d1
Pranay <pranaygodghate13@gmail.com>
2025-11-03 19:30:05 +0530

    First commit

---
+++ src/main/java/com/publicsapient/publicsapient/PublicsapientApplication.java
@@ -0,0 +1,13 @@
+package com.publicsapient.publicsapient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicsapientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicsapientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/publicsapient/publicsapient/PublicsapientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicsapient.publicsapient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicsapientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
